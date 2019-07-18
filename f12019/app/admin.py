d0ce7c68570004ef312e29497dfbe34744c3ecae
AndersonBatalha <andersonpbatalha@gmail.com>
2019-07-18 17:36:18 -0300

    Adicionados os models a página de administração do Django

--- f12019/app/admin.py
+++ f12019/app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Categoria_Post)
+admin.site.register(Cidade)
+admin.site.register(Circuito)
+admin.site.register(Comentario)
+admin.site.register(Equipe)
+admin.site.register(Evento)
+admin.site.register(Funcao)
+admin.site.register(Grupo)
+admin.site.register(MembrosGrupo)
+admin.site.register(Pais)
+admin.site.register(Piloto)
+admin.site.register(Pontuacao)
+admin.site.register(Post)
+admin.site.register(Relacionamento)
+admin.site.register(Resultado)
+admin.site.register(Titulo)
+admin.site.register(Usuario)
